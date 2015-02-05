e9c681023e86bc57b450336db8c580b9a9fc7ba5
BenjaminPeter <bpeter@uchicago.edu>
2015-02-04 19:58:34 -0600

    currently running version of posterior predictive inference, sloooooooooooow

--- posterior_predictive/posterior_predictive_draft.r
+++ posterior_predictive/posterior_predictive_draft.r
@@ -3,8 +3,8 @@ require(snpStats) #for read.plnk function
 #source("wishart.r")
 
 
-project.folder = "/data/eems-project/human_origins/projects/europe/input/europe"
-delta.folder = "/data/eems-project/human_origins/projects/europe/output/300_1/"
+#project.folder = "/data/eems-project/human_origins/projects/europe/input/europe"
+#delta.folder = "/data/eems-project/human_origins/projects/europe/output/300_1/"
 
 
 
@@ -16,6 +16,11 @@ get.snp.mat <- function( snp.id, data){
     snp <- as(snp, "numeric")[,1]
     snp[is.na(snp)] <- 0
     outer(snp, snp, function(x,y)(x-y)*(x-y))
+}
+
+get.snp.meta <- function( snp.id, data){
+    snp <- data$map[snp.id,]
+    return(c(snp$chromosome, snp$position))
 }
 
 subset.plink = function(data, indiv){
@@ -62,7 +67,7 @@ get.obs.matrices <- function(n.posterior.samples=10){
 }
 
 
-do.full.analysis <- function(max.n.snp=1, tot.n.snp=600000,
+old.do.full.analysis <- function(max.n.snp=1, tot.n.snp=600000,
                              n.posterior.samples=10){
     indiv <- read.table(paste0(project.folder, "europe.order"))
     diffs <- read.table(paste0(project.folder, "europe.diffs"))
@@ -105,7 +110,8 @@ d.eems <- function(diffs, deltas, df, sigma2, n.sims){
 L <- function(x){
     if (!is.null(dim(x) ))
         x = ncol(x)
-    cbind(-1, diag(rep(1, x-1)))
+    mat <- cbind(-1, diag(rep(1, x-1)))
+    Matrix(mat, sparse=TRUE)
 }
 
 log.det <- function(M){
@@ -135,24 +141,158 @@ load.files <- function(bed.file='chr2', project.folder,
     diffs <- read.table(paste0(project.folder, ".diffs"))
     diffs <- as.matrix(diffs)
 
-    thetas <- read.table(paste0(delta.folder, "mcmcthetas.txt"))
-    ll <- read.table(paste0(delta.folder, "mcmcpilogl.txt"))[,2]
-    sigma2 <- thetas[,1]
-    df <- thetas[,2]
-    df.rm <- get.df.rm(df, df.mode, tot.n.snp=tot.n.snp)
     
+    proj.files <- load.proj.files(project.folder)
+    delta.files <- load.delta.files(delta.folder)
 
-    data <- load.snp.data(bed.file=bed.file, indiv=indiv, ...)
+    df.rm <- get.df.rm(delta.files$df, df.mode, tot.n.snp=tot.n.snp)
+
+    data <- load.snp.data(bed.file=bed.file, indiv=proj.files$indiv, ...)
     print("loaded data")
 
     return(list("data"=data,
-                "diffs"=diffs,
+                "diffs"=proj.files$diffs,
+                "sigma2"=delta.files$sigma2,
+                "df"=delta.files$df,
+                "df.rm"=df.rm,
+                "deltas"=delta.files$deltas,
+                "ll"=delta.files$ll))
+}
+
+load.proj.files <- function(project.folder){
+    indiv <- read.table(paste0(project.folder, ".order"))
+    diffs <- read.table(paste0(project.folder, ".diffs"))
+    diffs <- as.matrix(diffs)
+    return(list("indiv"=indiv,
+                "diffs"=diffs))
+}
+
+load.delta.files <- function(delta.folder, max.n.samples=10000){
+    thetas <- read.table(paste0(delta.folder, "/mcmcthetas.txt"))
+    ll <- read.table(paste0(delta.folder, "/mcmcpilogl.txt"))[,2]
+    to.keep <- ll != 0
+    ll <- ll[to.keep]
+    sigma2 <- thetas[to.keep,1]
+    df <- thetas[to.keep,2]
+
+    n.samples.available <- sum(to.keep)
+
+    if(n.samples.available > max.n.samples){
+        n.samples <- max.n.samples
+        ll <- ll[1:n.samples]
+        sigma2 <- sigma2[1:n.samples]
+        df <- df[1:n.samples]
+    } else{
+        n.samples <- n.samples.available
+    }
+    deltas <- load.posterior(delta.folder, n.samples)
+    print(c("loaded posterior", delta.folder))
+
+    return(list("ll"=ll,
+                "sigma2"=sigma2,
+                "df"=df,
+                "deltas"=deltas,
+                "n.samples"=n.samples))
+}
+
+
+load.files.multiple <-function(bed.file='chr2', folder,
+                               tot.n.snp=600000, df.mode=3, 
+                               n.posterior.samples=10000, ... ){
+    id <- basename(folder)
+    project.folder <- paste0(folder, "/input/", id)
+    output.folders <- paste0(folder, "/output/") 
+    delta.folders <- list.files(output.folders, pattern="???_run*", full.names=T)
+
+    ll=c()
+    sigma2=c()
+    df=c()
+    deltas=list()
+    n.samples.left <- n.posterior.samples
+    for(d.folder in delta.folders){
+        delta.files <- load.delta.files(d.folder, n.samples.left)
+        ll <- c(ll, delta.files$ll)
+        sigma2 <- c(sigma2, delta.files$sigma2)
+        df <- c(df, delta.files$df)
+        deltas <- c(deltas, delta.files$deltas)
+        print(c(n.samples.left, delta.files$n.samples))
+        n.samples.left <- n.samples.left - delta.files$n.samples
+        print(c(n.samples.left) )
+        if(n.samples.left <= 0){
+            print("got max posterior samples")
+            break;
+        }
+    }
+    proj.files <- load.proj.files(project.folder)
+    print(c("loaded proj", project.folder))
+    data <- load.snp.data(bed.file=bed.file, indiv=proj.files$indiv, ...)
+    print("loaded data")
+
+    df.rm <- get.df.rm(df, df.mode, tot.n.snp=tot.n.snp)
+
+    return(list("data"=data,
+                "diffs"=proj.files$diffs,
                 "sigma2"=sigma2,
                 "df"=df,
                 "df.rm"=df.rm,
-                "data"=data,
+                "deltas"=deltas,
                 "ll"=ll))
+    
+}
 
+do.folder.analysis <- function(bed.file='chr2', folder, 
+                             n.posterior.samples=500,
+                             max.n.snp=200, tot.n.snp=600000,
+                             df.mode=3, write.freq=100,...){
+    
+    f <- load.files.multiple(bed.file, folder,
+                   tot.n.snp, df.mode, n.posterior.samples, ...)
+
+
+    freqs <- get.freq(f$data)
+
+    precomputes <- precompute(f$diffs, f$deltas, mode=2)
+    f$deltas = NULL
+    print("precomputed matrices")
+
+    max.n.snp <- min(max.n.snp, dim(f$data$genotypes)[2])
+    print(c("max.n.snp is", max.n.snp))
+
+
+    out.name <- sprintf("%s/out_%s.all1.txt", folder, bed)
+    out.name.probs <- sprintf("%s/out_%s.probs1.txt", folder, bed)
+    print(c("out names are", out.name, out.name.probs))
+
+    opt <- c()
+    opt.meta <- c()
+    append <- F
+    for(i in 1:max.n.snp){
+        lllr <- lllr3(i, f$data, df=f$df, 
+                   sigma2=f$sigma2, precomputes=precomputes)
+        opt <- rbind(opt, lllr)
+        opt.meta <- rbind(opt.meta, 
+                          c(freqs[i], get.snp.meta(i, f$data)))
+        if( i %% write.freq == 0 || i == max.n.snp){
+            print(i)
+
+            probs <- apply(exp(opt), 1, harmonic.mean)
+            probs <- cbind(probs, rowMeans(exp(opt)))
+            probs <- cbind(opt.meta, probs)
+
+            write.table(probs, out.name.probs, quote=F, col.names=F, 
+                        row.names=F, append=append)
+            write.table(opt, out.name, quote=F, col.names=F, 
+                        row.names=F, append=append)
+
+
+            if(i == write.freq){
+                append <- T
+            }
+            opt <- c()
+            opt.meta <- c()
+        }
+    }
+    return(list(opt, opt.meta))
 }
 
 do.full.analysis <- function(bed.file='chr2', project.folder, 
@@ -160,12 +300,10 @@ do.full.analysis <- function(bed.file='chr2', project.folder,
                              n.posterior.samples=100,
                              max.n.snp=200, tot.n.snp=600000,
                              df.mode=1, ...){
-
+    
     f <- load.files(bed.file, project.folder, delta.folder,
                    tot.n.snp, df.mode)
 
-    deltas <- load.posterior(delta.folder, n.posterior.samples)
-    print("loaded posterior")
 
     precomputes <- precompute(f$diffs, deltas)
     print("precomputed matrices")
@@ -181,22 +319,28 @@ do.full.analysis <- function(bed.file='chr2', project.folder,
                    sigma2=f$sigma2, precomputes=precomputes,
                    n.snps=tot.n.snp)
     return(lllr)
-
 }
 
 
-precompute <- function(diffs, deltas){
+precompute <- function(diffs, deltas, mode=2){
     mat <- list()
     LD <- L(diffs)
     LDt <- t(LD)
-    deltasl = lapply(deltas, function(Delta)-LD %*% Delta %*% LDt)
     diffsl = - LD %*% diffs %*% LDt
     mat$ldet.diffs <- log.det(diffsl)
-    mat$inv.diffs <- solve(diffsl)
+    mat$p <- nrow(diffsl)
 
+    if(mode==1){
+    deltasl = lapply(deltas, function(Delta)-LD %*% Delta %*% LDt)
     mat$ldet.deltas <- sapply(deltasl, log.det)
+    }
 
-    mat$inv.deltas <- lapply(deltasl, solve)
+    if(mode==2){
+        mat$ldiffsl <- diffsl
+        mat$diffs <- diffs
+    }
+
+    mat$inv.deltas <- lapply(deltas, function(x)solve(LDL(x)))
 
     mat$tr.invd.diffs <- sapply(mat$inv.deltas, trace.product, diffsl)
     mat
@@ -208,11 +352,14 @@ LDL <- function(d2){
 lllr.wishart <- function(snp, diffs, df, df.rm, sigma2,
                          precomputes,
                          n.snps=600000){
+    if(is.null(dim(snp))){stop("not valid snp matrix")}
+
     mat <- precomputes
     p <- dim(mat$inv.diffs)[1]
     delta.df <- df - df.rm
 
     snpn <- snp/n.snps
+    diffs <- diffs*(1-1/n.snps)
     d2 <- LDL(diffs-snpn)
     #d2 <- as.matrix(forceSymmetric(d2))
     log.det.d2 <- log.det(d2)
@@ -240,6 +387,7 @@ lllr.wishart <- function(snp, diffs, df, df.rm, sigma2,
     #det data term
     det.data.1 <- log.det.d2 * (df.rm - p - 1) /2 
     det.data <- mat$ldet.diffs * (df - p - 1) /2 
+    #print(sprintf("det_data:\t%0.10f\t%0.10f", det.data.1, det.data))
     lllratio <- lllratio + det.data.1 - det.data
 
     # detSigma term
@@ -250,15 +398,58 @@ lllr.wishart <- function(snp, diffs, df, df.rm, sigma2,
     obs.traces <- mat$tr.invd.diffs * df.rm/sigma2 /2 
     snp.traces <- sapply(mat$inv.deltas, trace.product, LDL(diffs-snpn))
     snp.traces <- snp.traces  * df/ sigma2 /2 
+    #print(sprintf("traces:\t%0.10f\t%0.10f", obs.traces, snp.traces))
     lllratio <- lllratio - snp.traces + obs.traces
     #snp.traces <- sapply(mat$inv.deltas, trace.product, LDL(snpn))
     #lllratio <- lllratio + df.rm/df * (1 - snp.traces/obs.traces)
 
     return(lllratio)
 }
+lllr.wishart2 <- function(snp, df, sigma2,
+                         precomputes,
+                         n.snps=600000, 
+                         normalize=F){
+    if(is.null(dim(snp))){stop("not valid snp matrix")}
+
+    mat <- precomputes
+    p <- mat$p
+
+    if(normalize){
+        target <- mean(precomputes$diffs)
+        x1 <- target * 1/n.snps
+        x2 <- target - x1
+        m1.norm <- mat$diffs * x2 / target
+        snp.norm <- snp / mean(snp) * x1
+        d2 <- LDL(m1.norm+snp.norm)
+    }else{
+        snpn <- snp/n.snps
+        lsnpn <- LDL(snpn)
+        d2 <- n.snps/(n.snps-1) * mat$ldiffsl - lsnpn
+    }
+    #d2 <- as.matrix(forceSymmetric(d2))
+    log.det.d2 <- log.det(d2)
+
+    #det data term
+    det.data.1 <- log.det.d2 * (df - p - 1) /2 
+    det.data <- mat$ldet.diffs * (df - p - 1) /2 
+
+    #traces term
+    data.traces <- mat$tr.invd.diffs * df/sigma2 /2 
+    data.1.traces <- sapply(mat$inv.deltas, trace.product, d2)
+    data.1.traces <- data.1.traces  * df/ sigma2 /2 
+
+    lllratio <- det.data.1 - det.data - data.1.traces + data.traces
+
+    return(lllratio)
+}
+
+lllr3 <- function(id, data, ...){
+    snp <- get.snp.mat(id, data)
+    lllr.wishart2(snp=snp, ...)
+}
 
 dWishart2 <- function(mat, df, sigma2, snp=NULL){
-    p <- nrow(mat$inv.diffs)
+    p <- mat$p
     if(is.null(snp)){
         ldX <- mat$ldet.diffs
         tr.Sigma.inv.X <- mat$tr.invd.diffs[[1]] * df /sigma2
@@ -298,18 +489,37 @@ harmonic.mean <- function(a){
 }
 
 load.posterior = function(eems.folder, max.matrices=NULL){
-    d <- read.table(paste0(eems.folder,"Delta.txt"), nrow=1)
+    d <- read.table(paste0(eems.folder,"/Delta.txt"), nrow=1)
     w <- ncol(d)
 
     if( is.null(max.matrices)){
-	d <- read.table(paste0(eems.folder,"Delta.txt"))
+	d <- read.table(paste0(eems.folder,"/Delta.txt"))
 	max.matrices = nrow(d) / w
     } else {
-	d <- read.table(paste0(eems.folder,"Delta.txt"), 
+	d <- read.table(paste0(eems.folder,"/Delta.txt"), 
 			nrow=w * max.matrices)
     }
 
     f = function(i)as.matrix(d[((i-1)*w+1):(i*w),])
     Delta = lapply(1:max.matrices, f)
     return(Delta)
+}
+
+
+args <- commandArgs(T)
+
+if(length(args)>1){
+    bed <- args[1]
+    folder <- args[2]
+    n.posterior.samples <- as.numeric(args[3])
+
+   fff <-do.folder.analysis(bed=args[1], folder=args[2],
+                         max.n.snp=100000,
+                         n.posterior.samples=n.posterior.samples) 
+
+   out.name <- sprintf("%s/out_%s.all.txt", folder, bed)
+   write.table(f, out.name, quote=F, col.names=F, row.names=F)
+   probs <- apply(exp(fff), 2, harmonic.mean)
+   out.name <- sprintf("%s/out_%s.probs.txt", folder, bed)
+   write.table(probs, out.name, quote=F, col.names=F, row.names=F)
 }
